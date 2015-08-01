56659fa7f913c8c828a27d7746d08940b0128ec1
aleju <kontakt@ajung.name>
2015-08-01 21:18:03 +0200

    Add: documentation to checks

--- check_laplotter.py
+++ check_laplotter.py
@@ -1,3 +1,10 @@
+"""Various checks to make sure that the LossAccPlotter() works decently.
+These checks do not run fully automatically, they have to be validated by
+a human.
+
+How to run them:
+    python check_laplotter.py
+"""
 from __future__ import print_function, division
 from laplotter import LossAccPlotter
 import matplotlib.pyplot as plt
@@ -8,7 +15,11 @@ def main():
     print("------------------")
     print("1 datapoint")
     print("------------------")
+    # generate example values for: loss train, loss validation, accuracy train
+    # and accuracy validation
     (loss_train, loss_val, acc_train, acc_val) = create_values(1)
+
+    # generate a plot showing the example values
     show_chart(loss_train, loss_val, acc_train, acc_val,
                title="A single datapoint")
 
@@ -41,7 +52,7 @@ def main():
     show_chart(np.array([]), np.array([]), acc_train, acc_val,
                lap=LossAccPlotter(show_loss_plot=False),
                title="150 datapoints, no loss chart")
-    
+
     print("")
     print("------------------")
     print("150 datapoints")
@@ -76,14 +87,13 @@ def main():
     print("150 datapoints")
     print("x-index 5 of loss_train should create a warning as its set to NaN")
     print("------------------")
-    # example values for loss and accuracy
     (loss_train, loss_val, acc_train, acc_val) = create_values(150)
 
     loss_train[5] = float("nan")
     
     show_chart(loss_train, loss_val, acc_train, acc_val,
                title="150 datapoints, one having value NaN (loss train at x=5)")
-    
+
     print("")
     print("------------------")
     print("1000 datapoints training")
@@ -91,8 +101,6 @@ def main():
     print("------------------")
     nb_points_train = 1000
     nb_points_val = 100
-
-    # example values for loss and accuracy
     (loss_train, loss_val, acc_train, acc_val) = create_values(nb_points_train)
 
     # set 9 out of 10 values of the validation arrays to -1.0 (Which will be
@@ -109,6 +117,19 @@ def main():
                title="1000 training datapoints, but only 100 validation datapoints")
 
 def create_values(nb_points):
+    """Generate example (y-)values for all lines with some added random noise.
+    
+    Args:
+        nb_points: Number of example values
+    Returns:
+        4 numpy arrays of values as a tuple: (array, array, array, array)
+        First Array: loss train
+        Second Array: loss validation
+        Third Array: accuracy train
+        Fourth Array: accuracy validation
+    """
+    # we add a bit more noise (0.1) to the validation data compared to the
+    # training data (0.05), which seems more realistic
     lt = add_noise(np.linspace(0.8, 0.1, num=nb_points), 0.05)
     lv = add_noise(np.linspace(0.7, 0.2, num=nb_points), 0.1)
     at = add_noise(np.linspace(0.5, 0.85, num=nb_points), 0.05)
@@ -116,9 +137,27 @@ def create_values(nb_points):
     return (lt, lv, at, av)
 
 def add_noise(values, scale):
+    """Add normal distributed noise to an array.
+    Args:
+        values: Numpy array of values, e.g. [0.7, 0.6, ...]
+        scale: Standard deviation of the normal distribution.
+    Returns:
+        Input array with added noise.
+    """
     return values + np.random.normal(scale=scale, size=values.shape[0])
 
 def show_chart(loss_train, loss_val, acc_train, acc_val, lap=None, title=None):
+    """Shows a plot using the LossAccPlotter and all provided values.
+
+    Args:
+        loss_train: y-values of the loss function of the training dataset.
+        loss_val: y-values of the loss function of the validation dataset.
+        acc_train: y-values of the accuracy of the training dataset.
+        acc_val: y-values of the accuracy of the validation dataset.
+        lap: A LossAccPlotter-Instance or None. If None then a new LossAccPlotter
+            will be instantiated. (Default is None.)
+        title: The title to use for the plot, i.e. LossAccPlotter.title .
+    """
     lap = LossAccPlotter() if lap is None else lap
     if title is not None:
         lap.title = title
@@ -137,7 +176,8 @@ def show_chart(loss_train, loss_val, acc_train, acc_val, lap=None, title=None):
         lap.add_values(idx, acc_val=av, redraw=False)
 
     lap.redraw()
-    
+
+    # block at the end so that the plot does not close immediatly.
     print("Close the chart to continue.")
     lap.block()
 
